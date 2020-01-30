ec19e72bb14f9edf03269a95555ca42f82f62bdd
rrbyn <55434239+rrbyn@users.noreply.github.com>
2020-01-30 10:12:22 +0200

    Algkoodi lisamine
    Algkoodi lisamine milles on algne tulu kulu kalkulaator

---
+++ app.js
@@ -0,0 +1,22 @@
+// 1. Get input values
+const first = document.querySelector("#first");
+const second = document.querySelector("#second");
+const btntulu = document.querySelector("#tulu");
+const btnkulu = document.querySelector("#kulu");
+const answer = document.querySelector("#answer");
+var currentvalue = 0
+// 2. Create calc function
+answer.innerText = currentvalue
+function tulu() {
+    // 4. Display output to user
+    currentvalue = currentvalue + parseInt(first.value)
+    answer.innerText = currentvalue;
+}
+function kulu() {
+    // 4. Display output to user
+    currentvalue = currentvalue - parseInt(first.value)
+    answer.innerText = currentvalue;
+}
+// 3. Add Event listener to button
+btntulu.addEventListener("click", tulu);
+btnkulu.addEventListener("click", kulu);
