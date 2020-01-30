954bd89f32a886bf0d1d2e9be9e239b3fd3c5b75
Multimeedia kasutaja <multimeedia@ametikool.ee>
2020-01-30 15:49:13 +0200

    Buttons correctly labled and working

--- app.js
+++ app.js
@@ -1,26 +1,55 @@
+function showBottom(e) {
+    console.log(e);
+
+    const el = document.querySelector(e);
+    console.log(el);
+    if (el.classList.contains("input_keys")) {
+        el.classList.remove("input_keys");
+
+    } else {
+        el.classList.add("input_keys");
+    }
+}
 // input nrs
 const first = document.querySelector("#first");
 const homeinput = document.querySelector("#homeinput");
-const uncatinput = document.querySelector("#uncatinput");
+const otherinput = document.querySelector("#otherinput");
 const shoppinginput = document.querySelector("#shoppinginput");
-const autoinput = document.querySelector("autoinput");
-const kidsinput = document.querySelector("#kidsinput");
+const carinput = document.querySelector("#carinput");
+const travelinput = document.querySelector("#travelinput");
 // buttons
 const btntulu = document.querySelector("#tulu");
 const homebtnkulu = document.querySelector("#homekulu");
-const uncatbtnkulu = document.querySelector("#uncatkulu");
+const otherbtnkulu = document.querySelector("#otherkulu");
 const shoppingbtnkulu = document.querySelector("#shoppingkulu");
-const billsbtnkulu = document.querySelector("#billskulu");
-const autobtnkulu = document.querySelector("#autokulu");
-const kidsbtnkulu = document.querySelector("#kidskulu");
-// ?
+const carbtnkulu = document.querySelector("#carkulu");
+const travelbtnkulu = document.querySelector("#travelkulu");
+// totals
 const answer = document.querySelector("#totalanswer");
+const home = document.querySelector("#hometotal");
+const other = document.querySelector("#othertotal");
+const shopping = document.querySelector("#shoppingtotal");
+const car = document.querySelector("#cartotal");
+const travel = document.querySelector("#traveltotal");
+// vars
 var currentvalue = 0
-
-
-
-// 2. Create calc function
+var hometotalvar = 0
+var othertotalvar = 0
+var shoppingtotalvar = 0
+var cartotalvar = 0
+var traveltotalvar = 0
+// init values
 answer.innerText = currentvalue
+home.innerText = hometotalvar
+other.innerText = othertotalvar
+shopping.innerText = shoppingtotalvar
+car.innerText = cartotalvar
+travel.innerText = traveltotalvar
+
+
+
+
+
 function tulu() {
     currentvalue = currentvalue + parseInt(first.value)
     answer.innerText = currentvalue;
@@ -28,33 +57,39 @@ function tulu() {
 function homekulu() {
     currentvalue = currentvalue - parseInt(homeinput.value)
     answer.innerText = currentvalue;
+    hometotalvar = hometotalvar + parseInt(homeinput.value)
+    home.innerText = hometotalvar;
 }
-function uncatkulu() {
-    currentvalue = currentvalue - parseInt(uncatinput.value)
+function otherkulu() {
+    currentvalue = currentvalue - parseInt(otherinput.value)
     answer.innerText = currentvalue;
+    othertotalvar = othertotalvar + parseInt(otherinput.value)
+    other.innerText = othertotalvar;
 }
 function shoppingkulu() {
     currentvalue = currentvalue - parseInt(shoppinginput.value)
     answer.innerText = currentvalue;
+    shoppingtotalvar = shoppingtotalvar + parseInt(shoppinginput.value)
+    shopping.innerText = shoppingtotalvar;
 }
-function billskulu() {
-    currentvalue = currentvalue - parseInt(billsinput.value)
+function carkulu() {
+    currentvalue = currentvalue - parseInt(carinput.value)
     answer.innerText = currentvalue;
+    cartotalvar = cartotalvar + parseInt(carinput.value)
+    car.innerText = cartotalvar;
 }
-function autokulu() {
-    currentvalue = currentvalue - parseInt(autoinput.value)
+function travelkulu() {
+    currentvalue = currentvalue - parseInt(travelinput.value)
     answer.innerText = currentvalue;
-}
-function kidskulu() {
-    currentvalue = currentvalue - parseInt(kidsinput.value)
-    answer.innerText = currentvalue;
+    traveltotalvar = traveltotalvar + parseInt(travelinput.value)
+    travel.innerText = traveltotalvar;
 }
 
 // 3. Add Event listener to button
 btntulu.addEventListener("click", tulu);
 homebtnkulu.addEventListener("click", homekulu);
-uncatbtnkulu.addEventListener("click", uncatkulu);
+otherbtnkulu.addEventListener("click", otherkulu);
 shoppingbtnkulu.addEventListener("click", shoppingkulu);
-billsbtnkulu.addEventListener("click", billskulu);
-kidsbtnkulu.addEventListener("click", kidskulu);
+carbtnkulu.addEventListener("click", carkulu)
+travelbtnkulu.addEventListener("click", travelkulu);
 
