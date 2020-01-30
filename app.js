8c51f4f1aa9026a5c281ad37648f239cb437b470
Multimeedia kasutaja <multimeedia@ametikool.ee>
2020-01-30 13:21:39 +0200

    Algne kalkulaator töötamas

--- app.js
+++ app.js
@@ -1,25 +1,59 @@
-// 1. Get input values
+// input nrs
 const first = document.querySelector("#first");
-const second = document.querySelector("#second");
+const homeinput = document.querySelector("#homeinput");
+const uncatinput = document.querySelector("#uncatinput");
+const shoppinginput = document.querySelector("#shoppinginput");
+const autoinput = document.querySelector("autoinput");
+const kidsinput = document.querySelector("#kidsinput");
+// buttons
 const btntulu = document.querySelector("#tulu");
-const homebtnkulu = document.querySelector("#kulu");
+const homebtnkulu = document.querySelector("#homekulu");
+const uncatbtnkulu = document.querySelector("#uncatkulu");
+const shoppingbtnkulu = document.querySelector("#shoppingkulu");
+const billsbtnkulu = document.querySelector("#billskulu");
+const autobtnkulu = document.querySelector("#autokulu");
+const kidsbtnkulu = document.querySelector("#kidskulu");
+// ?
 const answer = document.querySelector("#totalanswer");
 var currentvalue = 0
-kulu.visible = false
+
 
 
 // 2. Create calc function
 answer.innerText = currentvalue
 function tulu() {
-    // 4. Display output to user
     currentvalue = currentvalue + parseInt(first.value)
     answer.innerText = currentvalue;
 }
-function kulu() {
-    // 4. Display output to user
-    currentvalue = currentvalue - parseInt(first.value)
+function homekulu() {
+    currentvalue = currentvalue - parseInt(homeinput.value)
     answer.innerText = currentvalue;
 }
+function uncatkulu() {
+    currentvalue = currentvalue - parseInt(uncatinput.value)
+    answer.innerText = currentvalue;
+}
+function shoppingkulu() {
+    currentvalue = currentvalue - parseInt(shoppinginput.value)
+    answer.innerText = currentvalue;
+}
+function billskulu() {
+    currentvalue = currentvalue - parseInt(billsinput.value)
+    answer.innerText = currentvalue;
+}
+function autokulu() {
+    currentvalue = currentvalue - parseInt(autoinput.value)
+    answer.innerText = currentvalue;
+}
+function kidskulu() {
+    currentvalue = currentvalue - parseInt(kidsinput.value)
+    answer.innerText = currentvalue;
+}
+
 // 3. Add Event listener to button
 btntulu.addEventListener("click", tulu);
-btnkulu.addEventListener("click", kulu);
+homebtnkulu.addEventListener("click", homekulu);
+uncatbtnkulu.addEventListener("click", uncatkulu);
+shoppingbtnkulu.addEventListener("click", shoppingkulu);
+billsbtnkulu.addEventListener("click", billskulu);
+kidsbtnkulu.addEventListener("click", kidskulu);
